89ef6def9d137e45b1edd70fc9aeff3747a3a939
dbislimi <dbislimi@student.42nice.fr>
2025-06-08 20:04:06 +0200

    first rendering in /game, no animations yet

--- srcs/frontend/app/src/pages/Game.tsx
+++ srcs/frontend/app/src/pages/Game.tsx
@@ -9,10 +9,19 @@ export default function Game(){
 		};
 		fetchData();
 	}, [])
-	
+	let ballx = 100 * 4;
+	let bally = 50 * 4;
 	return (
 		<>
-		
+			<div className="h-screen w-screen flex items-center justify-center">
+				<div className="relative h-100 w-200 border-5">
+					<div className="absolute size-3 bg-red-700 rounded-full"
+						style={{
+							top: `${bally}px`,
+							left: `${ballx}px`
+						}}></div>
+				</div>
+			</div>
 		</>
 	)
 }
