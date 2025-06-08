6e0ef7c71915467e3fdf1c97ca99ecf5e6506fd6
dbislimi <dbislimi@student.42nice.fr>
2025-06-08 23:05:32 +0200

    added play button

--- srcs/frontend/app/src/pages/Game.tsx
+++ srcs/frontend/app/src/pages/Game.tsx
@@ -1,27 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function Game(){
-	useEffect(() => {
-		const fetchData = async () => {
-			const response: Response = await fetch("http://localhost:3000/game")
-			const data = await response.json();
-			console.log(data);
-		};
-		fetchData();
-	}, [])
+export default function Game() {
+//  useEffect(() => {
+//    const fetchData = async () => {
+//      const response: Response = await fetch("http://localhost:3000/game");
+//      const data = await response.json();
+//      console.log(data);
+//    };
+//    fetchData();
+//  }, []);
+	const [state, setState] = useState(false);
+	const handleButton = () => {
+    setState(!state);
+  };
+
 	let ballx = 100 * 4;
 	let bally = 50 * 4;
-	return (
-		<>
-			<div className="h-screen w-screen flex items-center justify-center">
-				<div className="relative h-100 w-200 border-5">
-					<div className="absolute size-3 bg-red-700 rounded-full"
-						style={{
-							top: `${bally}px`,
-							left: `${ballx}px`
-						}}></div>
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="h-screen w-screen flex items-center justify-center">
+        <div className="relative h-100 w-200 border-5 flex flex-col items-center justify-center">
+          {state || <button
+		  	onClick={handleButton}
+            type="button"
+            className="z-10 bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded"
+          >
+            PLAY
+          </button>}
+          <div
+            className="absolute size-3 bg-red-700 rounded-full"
+            style={{
+              top: `${bally}px`,
+              left: `${ballx}px`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
 }
