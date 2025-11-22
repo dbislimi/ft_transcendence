6e9124c2ebb9e5f41a112cdc3cc34b07480483b3
bilal57070 <129061121+bilal57070@users.noreply.github.com>
2025-11-22 21:26:50 +0100

    fixed bug

--- srcs/frontend/app/src/pages/Registration.tsx
+++ srcs/frontend/app/src/pages/Registration.tsx
@@ -108,7 +108,7 @@ export default function Registration() {
           name: data.user?.name || info.name,
           email: data.user?.email || info.email,
         };
-        login(userData);
+        login(userData, data.token);
         navigate("/");
       } else {
         const errMsg = data.error || (data.raw ? data.raw : "Erreur serveur");
